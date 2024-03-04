e109495acf84c427d678bcc7d012a8f5ec1a8ce4
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-03-04 21:25:14 +0900

    Enhanced Input 플레이어 컨트롤러

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,4 +2,35 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+
+}
+
+//입력 매핑 컨텍스트 추가
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	//check를 사용하여 AuraContext 포인터 확인
+	//이 조건이 실패하면 실행을 중단
+	check(AuraContext);
+	
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	//Error : EnhancedInputSubsystems.h 헤더를 열 수 없음
+	//Solution : Build.cs 파일 내 PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	//EnhancedInput 입력, Binaries 폴더 삭제 후 uproject 우클릭 후 visual studio 프로젝트 생성 
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+
+}
