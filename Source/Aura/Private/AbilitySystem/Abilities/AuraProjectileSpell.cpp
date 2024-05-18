16897f3d101b1bfcc8c79aaf8fe0e7ab8a6cb937
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-05-18 13:48:39 +0900

    comment

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -8,24 +8,32 @@
 
 void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
+	//ActivateAbility의 기본 클래스 구현 호출
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
+	//참고: 여기에서 능력을 활성화하는 추가 기능을 추가할 수 있다.
 }
 
 void UAuraProjectileSpell::SpawnProjectile()
 {
+	//서버가 투사체를 생성할 권한이 있는지 확인 
 	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
+	//서버가 아니면 함수 종료
 	if (!bIsServer) return;
 
+	//CombatInterface를 캐스팅하여 가져옴
 	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
 	if (CombatInterface)
 	{
+		//전투 소켓 위치를 가져옴
 		const FVector SocketLocation = CombatInterface->GetCombatSocketLocation();
 
+		//스폰 트랜스폼 설정
 		FTransform SpawnTransform;
 		SpawnTransform.SetLocation(SocketLocation);
-		//TODO : Set the Projectile Rotation
+		//TODO : 투사체 회전 설정
 
+		//투사체를 지연 생성
 		AAuraProjectile* Projectile = GetWorld()->SpawnActorDeferred<AAuraProjectile>(
 			ProjectileClass,
 			SpawnTransform,
@@ -33,8 +41,9 @@ void UAuraProjectileSpell::SpawnProjectile()
 			Cast<APawn>(GetOwningActorFromActorInfo()),
 			ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
 
-		//TODO : Give the Projectile a Gameplay Effect Spec for causing Damage
+		//TODO : 투사체에 피해를 입히기 위한 게임플레이 효과 스펙 부여
 
+		//투사체 스폰 완료
 		Projectile->FinishSpawning(SpawnTransform);
 	}
 }
