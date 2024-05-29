3aef0feb045000b1695ab11d1ea5951c166ae383
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-05-29 13:10:57 +0900

    ExecCalcs - Capturing Attribute

--- Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
+++ Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+
 #include "CoreMinimal.h"
 #include "Abilities/Tasks/AbilityTask.h"
 #include "TargetDataUnderMouse.generated.h"
@@ -9,7 +10,7 @@
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignature, const FGameplayAbilityTargetDataHandle&, DataHandle);
 
 /**
- * 
+ * 이 Task는 AbilityTask 클래스를 상속하여 구현됨
  */
 UCLASS()
 class AURA_API UTargetDataUnderMouse : public UAbilityTask
@@ -17,15 +18,20 @@ class AURA_API UTargetDataUnderMouse : public UAbilityTask
 	GENERATED_BODY()
 	
 public:
+	//대상 데이터가 유효할 때 발생하는 델리게이트
 	UFUNCTION(BlueprintCallable, Category="Ability|Tasks", meta = (DisplayName = "TargetDataUnderMouse", HidePin = "OwningAbility", DefaultToSelf = "OwningAbility", BlueprintInternalUseOnly = "true"))
 	static UTargetDataUnderMouse* CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility);
 
+	//새로운 UTargetDataUnderMouse 객체를 생성하여 반환
 	UPROPERTY(BlueprintAssignable)
 	FMouseTargetDataSignature ValidData;
 
 private:
 	virtual void Activate() override;
+	
+	//마우스 커서 아래의 데이터를 전송하는 함수
 	void SendMouseCursorData();
 
+	//리모트 플레이어로부터 대상 데이터가 복제될 때 호출되는 콜백 함수
 	void OnTargetDataReplicatedCallback(const FGameplayAbilityTargetDataHandle& DataHandle, FGameplayTag ActivationTag);
 };
