c0d7fefa724a6e922075d1f18f9fd8538fe2612c
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-06-09 13:31:22 +0900

    Combat Target

--- Source/Aura/Public/Interaction/EnemyInterface.h
+++ Source/Aura/Public/Interaction/EnemyInterface.h
@@ -24,4 +24,10 @@ class AURA_API IEnemyInterface
 public:
 	virtual void HighlightActor() = 0;
 	virtual void UnHighlightActor() = 0;
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void SetCombatTarget(AActor* InCombatTarget);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	AActor* GetCombatTarget() const;
 };
