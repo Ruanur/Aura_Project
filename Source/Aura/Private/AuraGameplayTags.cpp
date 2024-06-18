f64d0a7b449881919d85155c256af6bf741a5c68
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-06-18 15:58:44 +0900

    Demon Melee Attack

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -211,6 +211,10 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 		FString("Left Hand")
 		);
 
+	GameplayTags.CombatSocket_Tail = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("CombatSocket.Tail"),
+		FString("Tail")
+	);
 	/*
 	* Montage Tags
 	*/
