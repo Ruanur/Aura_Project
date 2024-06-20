cd3a22cc4d562107abcb20c80ef275851f8c7892
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-06-20 13:39:15 +0900

    Elementalist Attack Task

--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -75,4 +75,7 @@ public:
 
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	int32 GetMinionCount();
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void IncrementMinionCount(int32 Amount);
 };
