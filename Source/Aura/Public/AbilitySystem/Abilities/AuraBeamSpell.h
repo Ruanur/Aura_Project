94d3d0566c594decf1a7ab00409e59aa4bc8c5bf
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-09-04 16:32:45 +0900

    Electrocute Montage

--- Source/Aura/Public/AbilitySystem/Abilities/AuraBeamSpell.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraBeamSpell.h
@@ -19,7 +19,7 @@ public:
 	void StoreMouseDataInfo(const FHitResult& HitResult);
 
 	UFUNCTION(BlueprintCallable)
-	void StoreOwnerPlayerController();
+	void StoreOwnerVariables();
 
 protected:
 	UPROPERTY(BlueprintReadWrite, Category = "Beam")
@@ -30,4 +30,7 @@ protected:
 	
 	UPROPERTY(BlueprintReadWrite, Category = "Beam")
 	TObjectPtr<APlayerController> OwnerPlayerController;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	TObjectPtr<ACharacter> OwnerCharacter;
 };
